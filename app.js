e6b499390d0fae7fa5628a7e47868134a91f32b4
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-09-21 20:31:04 +0200

    standard --fix

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-'use strict';
+'use strict'
 
-const Homey = require('homey');
+const Homey = require('homey')
 
-const createRandomDinner = require('./lib/create-random-dinner');
+const createRandomDinner = require('./lib/create-random-dinner')
 
-const tokens = [];
+const tokens = []
 
 class MyApp extends Homey.App {
   /**
    * onInit is called when the app is initialized.
    */
-  async onInit() {
-    this.log(Homey.manifest.name.en + ' v' + Homey.manifest.version + ' is running...');
+  async onInit () {
+    this.log(Homey.manifest.name.en + ' v' + Homey.manifest.version + ' is running...')
 
     // Flow tokens
     new Homey.FlowToken('random_dinner', { type: 'string', title: Homey.__('flowTokens.random_dinner') })
       .register()
-      .then(token => tokens.push(token));
+      .then(token => tokens.push(token))
 
     // Actions
     new Homey.FlowCardAction('create_random_dinner')
       .register()
       .registerRunListener(async (args, state) => {
-        const ingredientCount = args.ingredient_count;
-        const allowToiletWords = (args.allow_toilet_words === 'true');
-        const allowIngredientMultipleTimes = (args.allow_ingredient_multiple_times === 'true');
+        const ingredientCount = args.ingredient_count
+        const allowToiletWords = (args.allow_toilet_words === 'true')
+        const allowIngredientMultipleTimes = (args.allow_ingredient_multiple_times === 'true')
 
-        tokens[0].setValue(createRandomDinner(ingredientCount, allowToiletWords, allowIngredientMultipleTimes));
+        if (Array.isArray(tokens) && tokens.length === 1) {
+          tokens[0].setValue(createRandomDinner(ingredientCount, allowToiletWords, allowIngredientMultipleTimes))
+        }
 
-        return Promise.resolve(true);
-      });
+        return true
+      })
   }
 }
 
-module.exports = MyApp;
+module.exports = MyApp
